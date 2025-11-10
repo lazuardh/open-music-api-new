21a20b5a191405dc7d5040e2f652d9fdb11c254b
lazuardh <habiyanlazuarad21@gmail.com>
2025-11-10 19:11:41 +0700

    feat: create api for albums

---
+++ src/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/inMemory/albumsService');
+const albumsValidator = require('./validator/albums');
+
+const ClientError = require("./exeption/clientError");
+
+
+const init = async () => {
+    const albumsService = new AlbumsService();
+
+    const host = process.env.HOST || '127.0.0.1';
+    const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+    const server = Hapi.server({
+        port: port,
+        host: host,
+        routes: {
+            cors: {
+                origin: ['*'],
+            }
+        }
+    });
+
+    //adding all pluggin
+    await server.register({
+        plugin: albums,
+        options: {
+            service: albumsService,
+            validator: albumsValidator,
+        }
+    });
+
+    //menambahkan onPreResponse untuk handle custom error
+    server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+      return h.continue;
+    });
+
+    await server.start();
+     console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
---
+++ src/validator/albums/index.js
@@ -0,0 +1,15 @@
+const { AlbumPayloadSchema } = require('./schema');
+const InvariantError = require('../../exeption/invariantError');
+
+/* this file will focus on create a funtion as a validator that uses schema  */
+const AlbumValidator = {
+    validateAlbumPayload: (payload) => {
+        const validateResult = AlbumPayloadSchema.validate(payload);
+        if (validateResult.error) {
+            throw new InvariantError(validateResult.error.message);
+        }
+
+    },
+};
+
+module.exports = AlbumValidator;
