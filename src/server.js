c46c3d0a114c102a7ad4762b721c470eadb5fdb9
lazuardh <habiyanlazuarad21@gmail.com>
2025-11-10 22:03:05 +0700

    feat: adding api for module songs, like create CRUD

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
 const albums = require('./api/albums');
 const AlbumsService = require('./services/inMemory/albumsService');
 const albumsValidator = require('./validator/albums');
+
+const songs = require('./api/songs');
+const SongsService = require('./services/inMemory/songsService');
+const songsValidator = require('./validator/songs');
 
 const ClientError = require("./exeption/clientError");
 
 
 const init = async () => {
     const albumsService = new AlbumsService();
+    const songsService = new SongsService();
 
     const host = process.env.HOST || '127.0.0.1';
     const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
@@ -24,13 +30,22 @@ const init = async () => {
     });
 
     //adding all pluggin
-    await server.register({
+    await server.register([
+      {
         plugin: albums,
         options: {
             service: albumsService,
             validator: albumsValidator,
         }
-    });
+      },
+      {
+        plugin: songs,
+        options: {
+          service: songsService,
+          validator: songsValidator,
+        },
+      },
+    ]);
 
     //menambahkan onPreResponse untuk handle custom error
     server.ext("onPreResponse", (request, h) => {
